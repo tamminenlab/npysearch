03ae443586e46e442e04226c2b2796fe7725a5b4
Aditya Jeevannavar <aditya.jeevan@gmail.com>
2021-12-03 14:08:46 +0200

    Modified it to setup both the C++ sub-module and the Python wrapper module

--- setup.py
+++ setup.py
@@ -3,29 +3,30 @@ import sys
 from pybind11 import get_cmake_dir
 # Available at setup time due to pyproject.toml
 from pybind11.setup_helpers import Pybind11Extension, build_ext
-from setuptools import setup
+from setuptools import setup, find_packages
 
 __version__ = "0.0.1"
 
 ext_modules = [
-    Pybind11Extension("npysearch",
-        ["src/Search.cpp", "src/TextReader.cpp"],
+    Pybind11Extension("_npysearch",
+        ["src/_npysearch/Search.cpp", "src/_npysearch/TextReader.cpp"],
         define_macros = [('VERSION_INFO', __version__)],
-        include_dirs=["include"]
+        include_dirs  = ["include"]
         ),
 ]
 
 setup(
-    name="npysearch",
-    version=__version__,
-    author="Aditya Jeevannavar",
-    author_email="adjeev@utu.fi",
-    url="https://github.com/jeevannavar/npysearch",
-    description="A test project to implement python bindings for nsearch",
-    long_description="",
-    ext_modules=ext_modules,
-    extras_require={"test": "pytest"},
-    cmdclass={"build_ext": build_ext},
-    zip_safe=False,
-    python_requires=">=3.6",
+    name             = "npysearch",
+    version          = __version__,
+    author           = "Aditya Jeevannavar",
+    author_email     = "adjeev@utu.fi",
+    url              = "https://github.com/jeevannavar/npysearch",
+    description      = "A test project to implement python bindings for nsearch",
+    long_description = "",
+    packages         = ["npysearch"],
+    ext_modules      = ext_modules,
+    extras_require   = {"test": "pytest"},
+    cmdclass         = {"build_ext": build_ext},
+    zip_safe         = False,
+    python_requires  = ">=3.6",
 )
