6519c05bbb075c46688850faf3ab8b13ef69cdaf
Aditya Jeevannavar <aditya.jeevan@gmail.com>
2021-12-03 14:09:25 +0200

    Removing moved files

--- src/Filter.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-extern bool DoFilter( const std::string& inputPath,
-                      const std::string& outputPath,
-                      const float        maxExpectedErrors );
--- src/Merge.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string>
-
-extern bool DoMerge( const std::string& fwdPath, const std::string& revPath,
-                     const std::string& mergedPath );
