facdedcee68599069f15619ebe7665c9262ab141
Aditya Jeevannavar <aditya.jeevan@gmail.com>
2021-12-09 16:36:44 +0200

    Corrected classifiers

--- setup.py
+++ setup.py
@@ -26,17 +26,20 @@ classifiers=[
     'Development Status :: 5 - Production/Stable',
 
     # Indicate who your project is intended for
-    'Intended Audience :: Bioinformaticians',
-    'Topic :: Bioinformatics :: BLAST Search',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
 
     # Pick your license as you wish (should match "license" above)
-    'License :: OSI Approved :: BSD-3-Clause License',
+    'License :: OSI Approved :: BSD License',
 
     # Specify the Python versions you support here. In particular, ensure
     # that you indicate whether you support Python 2, Python 3 or both.
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6'
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
 ]
 
 setup(
