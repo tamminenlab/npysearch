6f000a0406f48f472bafb52d25f37986287b3954
Aditya Jeevannavar <aditya.jeevan@gmail.com>
2021-12-09 14:56:17 +0200

    Changed version to 1.0.0 and added more metadata

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pybind11 import get_cmake_dir
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup, find_packages
 
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 
 ext_modules = [
     Pybind11Extension("_npysearch",
@@ -17,6 +17,27 @@ ext_modules = [
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+
+classifiers=[
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 5 - Production/Stable',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Bioinformaticians',
+    'Topic :: Bioinformatics :: BLAST Search',
+
+    # Pick your license as you wish (should match "license" above)
+    'License :: OSI Approved :: BSD-3-Clause License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6'
+]
 
 setup(
     name             = "npysearch",
@@ -33,4 +54,6 @@ setup(
     cmdclass         = {"build_ext": build_ext},
     zip_safe         = False,
     python_requires  = ">=3.6",
+    classifiers      = classifiers,
+    keywords         = "BLAST nsearch bioinformatics"
 )
